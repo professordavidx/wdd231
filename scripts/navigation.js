0ef4758272decdc17b5c8d79e77fecdf23006832
professordavidx <professordavidgomez@gmail.com>
2026-05-09 17:25:05 -0500

    site complete

---
+++ scripts/navigation.js
@@ -0,0 +1,7 @@
+const menuButton = document.querySelector('#menu-button');
+const navigation = document.querySelector('.navigation');
+
+menuButton.addEventListener('click', () => {
+  navigation.classList.toggle('open');
+  menuButton.classList.toggle('open');
+});
