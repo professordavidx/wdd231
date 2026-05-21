5e018f72cc2fc163abe6a8b0a03190055330199a
professordavidx <professordavidgomez@gmail.com>
2026-05-20 21:08:52 -0500

    weather complete

--- week3/scripts/weather.js
+++ week3/scripts/weather.js
@@ -4,21 +4,30 @@ const weatherIcon = document.querySelector('#weather-icon');
 const captionDesc = document.querySelector('figcaption');
 
 const url =
-  'https://api.openweathermap.org/data/2.5/weather?lat=49.749992&lon=6.637143&units=metric&appid=8e425eb61be4011a017b0692c7bd384a';
+  'https://api.openweathermap.org/data/2.5/weather?lat=49.749992&lon=6.637143&units=imperial&appid=8e425eb61be4011a017b0692c7bd384a';
 
 async function apiFetch() {
   try {
     const response = await fetch(url);
     if (response.ok) {
       const data = await response.json();
-      console.log(data); // testing only
-      // displayResults(data); // uncomment when ready
+      // console.log(data);
+      displayResults(data);
     } else {
       throw Error(await response.text());
     }
   } catch (error) {
     console.log(error);
   }
+}
+
+function displayResults(data) {
+  currentTemp.innerHTML = `${data.main.temp}&deg;F`;
+  const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+  let desc = data.weather[0].description;
+  weatherIcon.setAttribute('src', iconsrc);
+  weatherIcon.setAttribute('alt', desc);
+  captionDesc.textContent = `${desc}`;
 }
 
 apiFetch();
